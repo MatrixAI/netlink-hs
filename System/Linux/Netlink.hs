2bd57066d59862dec6e3ce32c1d5a7700bd391c2
Ksenia <ksenia.portu@gmail.com>
2019-11-04 22:54:28 +0500

    Add function `putAttributes`

--- System/Linux/Netlink.hs
+++ System/Linux/Netlink.hs
@@ -74,64 +74,18 @@ data Attribute = Attribute AttributeHeader
   (Either B.ByteString (NE.NonEmpty Attribute))
   deriving (Show, Eq)
 
-
-
 isEmpty :: SG.Get Bool
 isEmpty = SG.isEmpty
 
 isNotEmpty :: SG.Get Bool
 isNotEmpty = not <$> SG.isEmpty
 
-attrAlign :: Int
-attrAlign = 4
-
-attrAlignRemainder :: Int -> Int
-attrAlignRemainder attrSize = attrSize `mod` attrAlign
-
-attrPadding :: Int -> Int
-attrPadding attrSize = attrAlign - attrAlignRemainder attrSize
-
--- inclusive of length
-attrHeaderSize :: Int
-attrHeaderSize = 4
-
-getAttrHeader :: Get (AttributeHeader, Int)
-getAttrHeader = SG.isolate attrHeaderSize $ do
-  attrSize <- fromIntegral <$> SG.getWord16host
-  attrType <- fromIntegral <$> SG.getWord16host
-  return (AttributeHeader attrType, attrSize - attrHeaderSize)
-
--- warning: body recursion
-getAttr :: Set AttributeType -> Get Attribute
-getAttr nestedAttrTypes = do
-  (attrHeader, attrPaySize) <- getAttrHeader
-  if (attributeType attrHeader) `Set.notMember` nestedAttrTypes then do
-    attrVal <- SG.getByteString attrPaySize
-    getAttrPadding attrPaySize
-    return $ Attribute attrHeader (Left attrVal)
-  else do
-    nestedAttrs <- SG.isolate attrPaySize $ getAttrs nestedAttrTypes
-    getAttrPadding attrPaySize
-    return $ case NE.nonEmpty nestedAttrs of
-      Just nestedAttrs' -> Attribute attrHeader (Right $ nestedAttrs')
-      Nothing           -> Attribute attrHeader (Left "")
-
-getAttrs :: Set AttributeType -> Get Attributes
-getAttrs nestedAttrTypes = whileM isNotEmpty $ getAttr nestedAttrTypes
-
-getAttrPadding :: Int -> Get ()
-getAttrPadding size = do
-  let remainder = attrAlignRemainder size
-  e <- SG.isEmpty
-  if not e && remainder /= 0
-  then SG.skip $ attrAlign - remainder
-  else return ()
-
 -- inclusive of length
 msgHeaderSize :: Int
 msgHeaderSize = 16
 
-getMsgHeader:: Get (MessageHeader, Int)
+type MessagePaySize = Int
+getMsgHeader:: Get (MessageHeader, MessagePaySize)
 getMsgHeader = SG.isolate msgHeaderSize $ do
   msgSize   <- fromIntegral <$> SG.getWord32host
   msgType   <- (toEnum . fromIntegral) <$> SG.getWord16host
@@ -139,7 +93,8 @@ getMsgHeader = SG.isolate msgHeaderSize $ do
   msgSeqNum <- fromIntegral <$> SG.getWord32host
   msgPid    <- fromIntegral <$> SG.getWord32host
   return
-    (MessageHeader { messageType = msgType
+    (MessageHeader
+    { messageType = msgType
     , messageFlags = msgFlags
     , messageSeqNum = msgSeqNum
     , messagePid = msgPid
@@ -178,23 +133,87 @@ putMsgHeader len (MessageHeader ty flags seqnum pid) = do
     SP.putWord32host seqnum
     SP.putWord32host pid
 
--- |'Put' a 'Map' of 'Attributes'
+attrAlign :: Int
+attrAlign = 4
+
+attrAlignRemainder :: Int -> Int
+attrAlignRemainder attrSize = attrSize `mod` attrAlign
+
+attrPadding :: Int -> Int
+attrPadding attrSize = attrAlign - attrAlignRemainder attrSize
+
+-- inclusive of length
+attrHeaderSize :: Int
+attrHeaderSize = 4
+
+-- /*
+--  *  <------- NLA_HDRLEN ------> <-- NLA_ALIGN(payload)-->
+--  * +---------------------+- - -+- - - - - - - - - -+- - -+
+--  * |        Header       | Pad |     Payload       | Pad |
+--  * |   (struct nlattr)   | ing |                   | ing |
+--  * +---------------------+- - -+- - - - - - - - - -+- - -+
+--  *  <-------------- nlattr->nla_len -------------->
+--  */
+
+-- #define NLA_ALIGNTO             4
+-- #define NLA_ALIGN(len)          (((len) + NLA_ALIGNTO - 1) & ~(NLA_ALIGNTO - 1))
+-- #define NLA_HDRLEN              ((int) NLA_ALIGN(sizeof(struct nlattr)))
+
+-- "Sometimes there could be Padding inserted to the attributes.
+-- Usually it'll be taken care by macro,
+-- but check alignments and padding if you face issue when parsing raw memory dump."
+
 putAttributes :: Attributes -> Put
 putAttributes = mapM_ putAttr
-  -- where
-  --   putAttr :: Attribute -> Put
-  --   putAttr (ty, value) = do
-  --       SP.putWord16host (fromIntegral $ BS.length value + 4)
-  --       SP.putWord16host (fromIntegral ty)
-  --       SP.putByteString value
-  --       when (BS.length value `mod` 4 /= 0) $ do
-  --         replicateM_ (4 - (BS.length value `mod` 4)) (SP.putWord8 0)
   where
     putAttr :: Attribute -> Put
-    -- putAttr (Attribute (AttributeHeader ty) (Right attribute)) = do
-    putAttr (Attribute (AttributeHeader ty) (Left value)) = do
-        SP.putWord16host (fromIntegral $ BS.length value + 4)
-        SP.putWord16host (fromIntegral ty)
-        SP.putByteString value
-        when (BS.length value `mod` 4 /= 0) $ do
-          replicateM_ (4 - (BS.length value `mod` 4)) (SP.putWord8 0)
+    -- (Either B.ByteString (NE.NonEmpty Attribute))
+    putAttr (Attribute header (Right  (attribute NE.:| []) )) = do
+      putAttrHeader header attrHeaderSize
+      putAttr attribute
+    putAttr (Attribute header (Left value)) = do
+      let size = BS.length value
+      putAttrHeader header size
+      SP.putByteString value
+      when (attrAlignRemainder size /= 0) $ do
+        replicateM_ (attrPadding size) (SP.putWord8 0)
+
+-- warning: body recursion
+getAttr :: Set AttributeType -> Get Attribute
+getAttr nestedAttrTypes = do
+  (attrHeader, attrPaySize) <- getAttrHeader
+  if (attributeType attrHeader) `Set.notMember` nestedAttrTypes then do
+    attrVal <- SG.getByteString attrPaySize
+    getAttrPadding attrPaySize
+    return $ Attribute attrHeader (Left attrVal)
+  else do
+    nestedAttrs <- SG.isolate attrPaySize $ getAttrs nestedAttrTypes
+    getAttrPadding attrPaySize
+    return $ case NE.nonEmpty nestedAttrs of
+      Just nestedAttrs' -> Attribute attrHeader (Right $ nestedAttrs')
+      Nothing           -> Attribute attrHeader (Left "")
+
+getAttrPadding :: Int -> Get ()
+getAttrPadding size = do
+  let remainder = attrAlignRemainder size
+  e <- SG.isEmpty
+  if not e && remainder /= 0
+  then SG.skip $ attrAlign - remainder
+  else return ()
+
+
+-- putAttrPadding
+
+getAttrs :: Set AttributeType -> Get Attributes
+getAttrs nestedAttrTypes = whileM isNotEmpty $ getAttr nestedAttrTypes
+
+putAttrHeader :: AttributeHeader -> Int -> Put
+putAttrHeader (AttributeHeader attrType) size = do
+  SP.putWord16host (fromIntegral $ size + attrAlign)
+  SP.putWord16host attrType
+
+getAttrHeader :: Get (AttributeHeader, Int)
+getAttrHeader = SG.isolate attrHeaderSize $ do
+  attrSize <- fromIntegral <$> SG.getWord16host
+  attrType <- fromIntegral <$> SG.getWord16host
+  return (AttributeHeader attrType, attrSize - attrHeaderSize)
